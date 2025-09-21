d51a74a28c35a6417400c3a0a1c53102ab2b5e63
lyna006 <pauporras@estudiantec.cr>
2025-09-21 12:09:03 -0600

    flags, game over and win

--- Interfaz.rkt
+++ Interfaz.rkt
@@ -12,7 +12,9 @@
 (define fila 8)
 (define column 8)
 (define mapa '())
-(define celdas-reveladas '()) ; matriz para controlar qué celdas están reveladas
+(define celdas-reveladas '()) ; #t si está revelada
+(define celdas-flags '())     ; #t si tiene bandera 🚩
+(define juego-terminado? #f)
 (define label-bombas #f)
 (define total-bombas 0)
 (define canvas #f)
@@ -25,8 +27,23 @@
     (for/list ([j (in-range cols)])
       #f)))
 
+(define (inicializar-flags filas cols)
+  (for/list ([i (in-range filas)])
+    (for/list ([j (in-range cols)]) #f)))
+
 ; ========================================
-; Revelar celda
+; Contador de banderas y actualización de label
+; ========================================
+(define (contar-banderas)
+  (for/sum ([fila celdas-flags])
+    (for/sum ([v fila]) (if v 1 0))))
+
+(define (actualizar-indicadores!)
+  (define k (contar-banderas))
+  (send label-bombas set-label (format "Bombas: ~a   |   🚩: ~a" total-bombas k)))
+
+; ========================================
+; Revelar / consultar estado
 ; ========================================
 (define (revelar-celda fila-idx col-idx)
   (when (and (< fila-idx (length celdas-reveladas))
@@ -46,6 +63,26 @@
   (and (< fila-idx (length celdas-reveladas))
        (< col-idx (length (first celdas-reveladas)))
        (list-ref (list-ref celdas-reveladas fila-idx) col-idx)))
+
+; ========================================
+; Game Over / Victoria
+; ========================================
+(define (revelar-todas-las-bombas!)
+  (define filas (length mapa))
+  (define cols  (length (first mapa)))
+  (for* ([i (in-range filas)] [j (in-range cols)])
+    (when (= 1 (car (list-ref (list-ref mapa i) j)))
+      (revelar-celda i j))))
+
+;; Ganas si todas las NO-bomba están reveladas
+(define (victoria?)
+  (define filas (length mapa))
+  (define cols  (length (first mapa)))
+  (for*/and ([i (in-range filas)] [j (in-range cols)])
+    (define celda (list-ref (list-ref mapa i) j))
+    (define es-bomba (= 1 (car celda)))
+    (define rev? (celda-revelada? i j))
+    (or es-bomba rev?)))
 
 ; ========================================
 ; Dibujar tablero
@@ -98,10 +135,19 @@
               (send dc draw-line x0 y0 x0 (+ y0 cellsize))
               (send dc set-pen "darkgray" 2 'solid)
               (send dc draw-line (+ x0 cellsize) y0 (+ x0 cellsize) (+ y0 cellsize))
-              (send dc draw-line x0 (+ y0 cellsize) (+ x0 cellsize) (+ y0 cellsize))))))))
+              (send dc draw-line x0 (+ y0 cellsize) (+ x0 cellsize) (+ y0 cellsize))
+              ; bandera si existe
+              (when (list-ref (list-ref celdas-flags i) j)
+                (define txt "🚩")
+                (send dc set-font (make-font #:size (round (* 0.6 cellsize)) #:weight 'bold))
+                (define-values (tw th _d _a) (send dc get-text-extent txt))
+                (define tx (+ x0 (/ (- cellsize tw) 2)))
+                (define ty (+ y0 (/ (- cellsize th) 2)))
+                (send dc set-text-foreground "red")
+                (send dc draw-text txt tx ty))))))))
 
 ; ========================================
-; Ventanas
+; Ventanas y menú
 ; ========================================
 (define frame (new frame% [label "Buscaminas"] [width Wancho] [height Walto]))
 (define Menuframe (new frame% [label "MENU - BUSCAMINAS"] [width 450] [height 350]))
@@ -117,23 +163,32 @@
 (define menu-filas
   (new choice% [parent size-panel] [label "Filas: "] [choices '("8" "9" "10" "11" "12" "13" "14" "15")]
        [font (make-font #:size 14 #:weight 'bold)]
-       [callback (lambda (c e) (set! fila (string->number (send c get-string (send c get-selection)))))]))
+       [callback (lambda (c e)
+                   (set! fila (string->number (send c get-string (send c get-selection)))))]))
+
 (define menu-columnas
-  (new choice% [parent size-panel] [label "Columnas: "] [choices '("8" "9" "10" "11" "12" "13" "14" "15")]
+  (new choice% [parent size-panel] [label "Columnas: "]
+       [choices '("8" "9" "10" "11" "12" "13" "14" "15")]
        [font (make-font #:size 14 #:weight 'bold)]
-       [callback (lambda (c e) (set! column (string->number (send c get-string (send c get-selection)))))]))
+       [callback (lambda (c e)
+                   (set! column (string->number (send c get-string (send c get-selection)))))]))
+
 (new message% [parent menu-panel] [label ""])
 (define dificultad-panel (new horizontal-panel% [parent menu-panel] [alignment '(center center)]))
 (new message% [parent dificultad-panel] [label "Dificultad"] [font (make-font #:size 16 #:weight 'bold)])
 (define botones-panel (new horizontal-panel% [parent menu-panel] [alignment '(center center)]))
 
-; Función iniciar juego
+; ========================================
+; Iniciar juego
+; ========================================
 (define (iniciar-juego nivel)
   (set! total-bombas (que_nivel nivel))
   (set! mapa (generar_mapa fila column total-bombas))
   (set! celdas-reveladas (inicializar-reveladas fila column))
-  (send label-bombas set-label (format "Bombas: ~a" total-bombas))
-; Ajustar tamaño de ventana y canvas dinámicamente
+  (set! celdas-flags (inicializar-flags fila column))
+  (set! juego-terminado? #f)
+  (actualizar-indicadores!)
+  ; ajustar tamaño dinámico
   (define new-width (* column cellsize))
   (define new-height (+ 60 (* fila cellsize))) 
   (send frame resize new-width new-height)
@@ -142,7 +197,6 @@
   (send Menuframe show #f)
   (send frame show #t)
   (send canvas refresh))
-
 
 (new button% [parent botones-panel] [label "Fácil"] [min-width 100]
      [font (make-font #:size 12 #:weight 'bold)] [callback (lambda (b e) (iniciar-juego 1))])
@@ -153,10 +207,18 @@
 
 ; Panel superior del juego
 (define top-panel (new horizontal-panel% [parent frame] [min-height 40]))
-(set! label-bombas (new message% [parent top-panel] [label "Bombas: 0   "] [font (make-font #:size 14 #:weight 'bold)]))
-(new button% [parent top-panel] [label "Reiniciar"] [callback (lambda (b e)
-                                                               (send frame show #f)
-                                                               (send Menuframe show #t))])
+(set! label-bombas (new message% [parent top-panel] [label "Bombas: 0   |   🚩: 0"]
+                        [font (make-font #:size 14 #:weight 'bold)]))
+(new button% [parent top-panel] [label "Reiniciar"]
+     [callback (lambda (b e)
+                 (set! mapa '())
+                 (set! celdas-reveladas '())
+                 (set! celdas-flags '())
+                 (set! juego-terminado? #f)
+                 (set! total-bombas 0)
+                 (send frame show #f)
+                 (send Menuframe show #t)
+                 (actualizar-indicadores!))])
 
 ; ========================================
 ; Canvas con clics funcionales
@@ -165,14 +227,41 @@
   (class canvas%
     (inherit get-dc)
     (define/override (on-event event)
-      (when (send event button-down?)
+      (define tipo (send event get-event-type)) ; 'left-down o 'right-down
+      (when (and (not juego-terminado?)
+                 (or (eq? tipo 'left-down) (eq? tipo 'right-down)))
         (define x (send event get-x))
         (define y (send event get-y))
-        (define col (quotient x cellsize))
-        (define fila (quotient y cellsize))
-        (when (and (< fila (length mapa))
-                   (< col (length (first mapa))))
-          (revelar-celda fila col)))
+        (define col-idx (quotient x cellsize))
+        (define row-idx (quotient y cellsize))
+        (when (and (< row-idx (length mapa))
+                   (< col-idx (length (first mapa))))
+          (cond
+            [(eq? tipo 'right-down) ; alternar bandera
+             (unless (celda-revelada? row-idx col-idx)
+               (set! celdas-flags
+                     (for/list ([i (in-range (length celdas-flags))])
+                       (for/list ([j (in-range (length (first celdas-flags)))])
+                         (if (and (= i row-idx) (= j col-idx))
+                             (not (list-ref (list-ref celdas-flags i) j))
+                             (list-ref (list-ref celdas-flags i) j))))))
+             (actualizar-indicadores!)
+             (send canvas refresh)]
+            [else ; left-down → revelar
+             (unless (list-ref (list-ref celdas-flags row-idx) col-idx)
+               (define celda (list-ref (list-ref mapa row-idx) col-idx))
+               (cond
+                 [(= 1 (car celda))
+                  (set! juego-terminado? #t)
+                  (revelar-todas-las-bombas!)
+                  (send canvas refresh)
+                  (message-box "Fin del juego" "¡Boom! Pisaste una bomba.")]
+                 [else
+                  (revelar-celda row-idx col-idx)
+                  (send canvas refresh)
+                  (when (victoria?)
+                    (set! juego-terminado? #t)
+                    (message-box "¡Ganaste!" "Has despejado todas las celdas. 🎉"))]))])))
       (super on-event event))
     (super-new)))
 
